fad974441f31f4d53eacb287de9bd8d45c0023c7
bewithsnehasish <mandalsneh97@gmail.com>
2024-03-11 22:10:35 +0530

    update

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import os
 
 def execute_distribution_scripts(distribution):
     # Define the paths to the distribution-specific scripts
-    fonts_script_path = f"./src/{distribution}/fonts.sh"
+    #fonts_script_path = f"./src/{distribution}/fonts.sh"
     browser_script_path = f"./src/{distribution}/browser.sh"
     custom_zsh_script_path = f"./src/{distribution}/CustomZsh.sh"
     
     # Check if the scripts exist
-    if os.path.exists(fonts_script_path):
-        subprocess.run(["kitty", fonts_script_path], text=True)
-    else:
-        print(f"Error: Fonts script not found for {distribution} distribution.")
+    # if os.path.exists(fonts_script_path):
+    #     subprocess.run(["kitty", fonts_script_path], text=True)
+    # else:
+    #     print(f"Error: Fonts script not found for {distribution} distribution.")
     
     if os.path.exists(browser_script_path):
         subprocess.run(["kitty", browser_script_path], text=True)
